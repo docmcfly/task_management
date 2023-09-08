9062a786d8a676bbd6151a6407a3737964a4a5a3
Clemens Gogoljn <service@cylancer.net>
2023-09-08 14:45:05 +0200

    Change the plugin description

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Task management',
-    'description' => 'is a simple task management: Create, delete, done, display (open and closed tasks)
-      changelog: 
-      2.2.3 :: Fix the missing jquery and a translation
-      2.2.1 :: Fix the plugin registration/configuration.
-      2.2.0 :: Release the bootsrap version 
-      2.0.x :: full migration to TYPO3 11.5  
-      1.3.0 :: update: make this plugin TYPO3 11.5 LTS ready
-      1.2.0 :: update: a user group is informed if a repeated task is added to the open tasks. 
-      1.1.0 :: update: you can repeat task define and an background batch job repeats the task.  
-      1.0.1 :: first productive version  
-',
+    'description' => 'is a simple task management: Create, delete, done, display (open and closed tasks)',
     'category' => 'plugin',
     'author' => 'Clemens Gogolin',
     'author_email' => 'service@cylancer.net',
@@ -27,3 +17,15 @@ $EM_CONF[$_EXTKEY] = [
         'suggests' => []
     ]
 ];
+
+
+/* CHANGLOG: 
+      2.2.3 :: Fix the missing jquery and a translation
+      2.2.1 :: Fix the plugin registration/configuration.
+      2.2.0 :: Release the bootsrap version 
+      2.0.x :: full migration to TYPO3 11.5  
+      1.3.0 :: update: make this plugin TYPO3 11.5 LTS ready
+      1.2.0 :: update: a user group is informed if a repeated task is added to the open tasks. 
+      1.1.0 :: update: you can repeat task define and an background batch job repeats the task.  
+      1.0.1 :: first productive version  
+ */
